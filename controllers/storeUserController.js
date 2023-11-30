2fa74837cfd13fdcf19372e5bafcf274dd02fa5b
kasin <kasin.thappawan@gmail.com>
2023-12-01 01:30:53 +0700

    update
    -ใส่ข้อมูลใน card หมดแล้ว
    - แมพสามารถใช้งานได้

--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -1,21 +1,32 @@
 const User = require('../models/User');
-//รับค่าจาก form ใน register มาหลังจากนั้นนำข้อมูลที่ได้รับไปเก็บไว้ใน database
-//ดึงข้อมูลมาจาก /models/User
-//หากมี error ให้แสดงข้อความ error ออกมายัง user
 
 module.exports = (req, res) => {
-    // รับข้อมูลมาจาก form email เพื่อตรวจสอบ
-    // ถ้าใน form เป็น hotmail และ outlook ให้แจ้ง alert ขึ้นมา และให้ redirect ในหน้า register
     const email = req.body.email;
+    const password = req.body.password;
+
+    // ตรวจสอบความยาวของ password
+    if (password.length <= 5) {
+        req.flash('validationErrors', 'Password ต้องมีมากกว่า 5 ตัวอักษร');
+        req.flash('data', req.body);
+        return res.redirect('/register');
+    }
+
+    // ตรวจสอบว่า password ไม่ใช่ตัวเลข 0-9
+    if (/^\d+$/.test(password)) {
+        req.flash('validationErrors', 'Password ต้องไม่เป็นตัวเลข 0-9');
+        req.flash('data', req.body);
+        return res.redirect('/register');
+    }
+
     const isHotmailOrOutlook = email.match(/@(hotmail\.com|outlook\.com)$/);
     const isGmail = email.match(/@gmail\.com$/);
-    //จับ error หาก user กรอก hotmail และ outlook ให้ขึ้น alert และไม่ให้บันทึกข้อมูลนี้ลง database
+
     if (isHotmailOrOutlook) {
         req.flash('validationErrors', 'ไม่สามารถลงทะเบียนด้วย Hotmail และ Outlook ได้');
         req.flash('data', req.body);
         return res.redirect('/register');
     }
-     //จับ error หาก user กรอกสิ่งที่ไม่ใช้ email เช่น test@sdfsdfsd.com เป็นต้น ให้ขึ้น alert และไม่ให้บันทึกข้อมูลนี้ลง database
+
     if (!isGmail) {
         req.flash('validationErrors', 'ให้ใช้ @Gmail.com เท่านั้น');
         req.flash('data', req.body);
